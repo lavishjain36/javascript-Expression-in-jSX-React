dc8c0c9bc95a05a63e976892b583f103f472fcd9
Lavish Jain <lavishjain36@gmail.com>
2021-01-26 00:33:21 +0530

    Jsx use in html {}

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import ReactDOM from "react-dom";
 import React from "react";
 
+//create a constant
+const name = "Lavish Jain";
+
 ReactDOM.render(
   <>
-    <h1>Welcome to React First App.</h1>
-    <p>List of Places :</p>
-    <ol>
-      <li>Pune</li>
-      <li>Mumbai</li>
-      <li>Delhi</li>
-      <li>Chennai</li>
-      <li>Bengaluru</li>
-    </ol>
+    <h1>This is {name} from Pune.</h1>
+    <p>Using javascript in Html .Add {10 + 20}</p>
+    <p>Using javascript in Html .Add {Math.random()}</p>
   </>,
 
   document.getElementById("root")
 );
+
+//Here we are writing javascript expression in JSX
+//using {} curly braces
