cd512d614224be18a1821be1831981065d921e7a
Lavish Jain <lavishjain36@gmail.com>
2021-01-26 00:14:25 +0530

    reactfirstapp

--- src/index.js
+++ src/index.js
@@ -1,22 +1,18 @@
-// var React = require("react");
-// var ReactDOM = require("react-dom");
 import ReactDOM from "react-dom";
-
 import React from "react";
 
-//its possible for render(0 to return an array of elements in react 16 vers or greater
 ReactDOM.render(
-  <React.Fragment>
-    <h1>Hello People. This is Lavish.</h1>
-    <p>I am from Pune</p>
-    <label htmlFor="">Enter Your Name</label>
-    <input type="text" />
-  </React.Fragment>,
+  <>
+    <h1>Welcome to React First App.</h1>
+    <p>List of Places :</p>
+    <ol>
+      <li>Pune</li>
+      <li>Mumbai</li>
+      <li>Delhi</li>
+      <li>Chennai</li>
+      <li>Bengaluru</li>
+    </ol>
+  </>,
+
   document.getElementById("root")
 );
-
-//React Fragment is same as div but it is faster than div.
-//it consume less memory
-//if we dont use React.Fragment ,we would not get error
-//Its is Special fragment Syntax
-//React 16.x is also a syntactic sugar fragment
